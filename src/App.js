60fcacf59a338c602433efbcf33916073c0368de
eltonaguiar77 <129380874+eltonaguiar77@users.noreply.github.com>
2025-04-01 07:49:56 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      titulo: "Primeira página com Class Component"
+    }
+
+    this.mudar = this.mudar.bind(this);
+  }
+
+  mudar(){
+    console.log("Mudou");
+    var novoTitulo = "Aprendi a usar Class Component";
+    this.setState({titulo:novoTitulo});
+  }
+
+  render(){
+    return(
+      <div>
+        <h1> {this.state.titulo} </h1>
+        <button onClick={this.mudar}>Mudar título</button>
+      </div>
+    )
+  }
 }
 
 export default App;
