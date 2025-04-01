9243ad08b6fdfe4a21a1580055d3c21a7105e507
eltonaguiar77 <129380874+eltonaguiar77@users.noreply.github.com>
2025-04-01 08:23:03 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import React from "react";
-import "./App.css"
+import React, {Component} from "react";
 
-const Nome = (props) => {
-  return(
-    <div className="estilo">
-      { props.texto }
-    </div>
-  )
-}
+class App extends Component{
 
+  constructor(props){
+    super(props);
 
-function App() {
-  return (
-    <div>
-      <Nome texto="Fridoca Sapeca"/>
-      <Nome texto="Giovanna Gualda"/>
-    </div>
-  );
+    this.state = {
+      mensagem: "Mensagem provisória",
+      email: "",
+      senha: ""
+    }
+
+    this.capturaDados = this.capturaDados.bind(this);
+    this.acessar = this.acessar.bind(this);
+  }
+
+  capturaDados = e =>{
+    console.log(e.target.value);
+    this.setState( { [e.target.name]: e.target.value } );
+  }
+
+  acessar(){
+    console.log("Acessou!");
+    var email = this.state.email;
+    var senha = this.state.senha;
+    if( email == "eltonaguiar@gmail.com" && senha == "123456"){
+      this.setState( { mensagem: "Acessado com sucesso!" } );
+    } else {
+      this.setState( { mensagem: "Usuário ou senha incorretos!" } );
+    }
+  }
+
+  render(){
+    const {emailInput} = this.state;
+    const {senhaInput} = this.state;
+    return(
+      <div>
+        <h1>Login</h1>
+        <label>
+          <input name="email" 
+                 value={emailInput}
+                 onChange={this.capturaDados} /><br></br>
+          <input name="senha" 
+                 value={senhaInput}
+                 onChange={this.capturaDados}/><br></br>
+          <button onClick={this.acessar}>Acessar</button>
+        </label>
+        <h4> { this.state.mensagem } </h4>
+      </div>
+    )
+  }
 }
 
 export default App;
